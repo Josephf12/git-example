acc61090488b0c949c7574fe28a8f9d4041995a9
JosephFalayi2 <>
2022-11-04 12:37:48 +0000

    Class task

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import random
 
 def generate_random_number():
-  choice= random.randint(1,10)
-  print(choice)
-
+  ran_num= random.randint(1,10)
+  print(ran_num)
+  def difference_from_answer(guess,answer):
+    answer=ran_num
+    if guess == answer:
+      return "correct"
+    elif guess > answer:
+        print("too High")
+    elif guess < answer:
+      print("too low")
+  difference_from_answer(5,ran_num)    
+    
 generate_random_number()
+
+#def difference_from_answer(guess,answer):
+  
+  
